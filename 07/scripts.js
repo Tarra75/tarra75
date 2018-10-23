473d44cde852284acc2e32668deae346f9bd1ac4
Tarra <tarra.boroumandi@gmail.com>
2018-10-23 09:29:04 -0400

    new

--- 07/scripts.js
+++ 07/scripts.js
@@ -9,7 +9,7 @@ const y = prompt('Enter a number');
 const z = prompt('Enter a number');
 const w = prompt('Enter a number');
 
-let message = x + ' + ' + y + ' - ' + z + ' x ' + w + ' = ' + (((Number(x) + Number(y)) - Number(z)) * Number(w));
+let message = '(' + x + ' + ' + y + ' - ' + z + ')' +' x ' + w + ' = ' + (((Number(x) + Number(y)) - Number(z)) * Number(w));
 
 
 console.log(message);
